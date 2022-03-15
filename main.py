5021a743bba313de5bac690ffd9bb2a0469b34b7
Ivaylo Rachev <rachev.ivaylo@yahoo.fr>
2022-03-15 12:14:37 +0100

    Ajout du fichier main.py
    Ajout du fichier main.py qui va appeler toutes les méthodes pour lancer l'application

---
+++ main.py
@@ -0,0 +1,8 @@
+from scrape_livres import scrapeLivres
+
+if __name__ == "__main__":
+    scraper = scrapeLivres()
+    scraper.prendre_toutes_categories()
+    scraper.prendre_tous_livres()
+    scraper.creer_livres_csv()
+    scraper.telecharger_images_livres()
